c3b1f97f1f024533aa00c1c49da42a5ba492fd26
Dhanesh <dnagarajan89@gmail.com>
2019-07-29 12:15:40 +0530

    Changes for adding thymeleaf templates

--- mwpl-anaylser/src/main/java/org/ta/dani/mwpl/nse/controller/MwplController.java
+++ mwpl-anaylser/src/main/java/org/ta/dani/mwpl/nse/controller/MwplController.java
@@ -1,26 +1,16 @@
 package org.ta.dani.mwpl.nse.controller;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.ta.dani.mwpl.excepion.DateAlreadyProcessedException;
-import org.ta.dani.mwpl.nse.model.CombinedVolAndOI;
-import org.ta.dani.mwpl.nse.processor.NseMwplProcessor;
 
-@RestController
-@RequestMapping("/mwpl-api")
+import io.swagger.models.Model;
+
+@Controller
 public class MwplController {
 
-	@Autowired
-	NseMwplProcessor nseMwplProcessor;
-
-	@GetMapping("/today")
-	List<CombinedVolAndOI> processMwplForToday() throws DateAlreadyProcessedException {
-		return nseMwplProcessor.processMwpl(LocalDate.now().minusDays(1), true);
+	@GetMapping("/mwpl")
+	public String hello(Model model) {
+		return "mwpl";
 	}
 
 }
