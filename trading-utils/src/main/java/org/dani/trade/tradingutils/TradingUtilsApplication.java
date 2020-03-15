0b99f443fc3a33ee6d2ca0b2767a14671c0e53f9
Dhanesh <dnagarajan89@gmail.com>
2020-03-15 23:26:40 +0530

    Adding a new project

---
+++ trading-utils/src/main/java/org/dani/trade/tradingutils/TradingUtilsApplication.java
@@ -0,0 +1,13 @@
+package org.dani.trade.tradingutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingUtilsApplication.class, args);
+	}
+
+}
---
+++ trading-utils/src/test/java/org/dani/trade/tradingutils/TradingUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dani.trade.tradingutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
